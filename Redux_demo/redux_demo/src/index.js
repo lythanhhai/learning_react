6fbae7377addb4b85ece31590d41b8425408144f
lythanhhai <you@example.com>
2021-10-07 09:49:14 +0700

    redux demo 1

---
+++ Redux_demo/redux_demo/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+
+
+// const increment = () => {
+//   return(
+//    {
+//     type: "increment",
+//    } 
+//   );
+// };
+// const decrement = () => {
+//   return(
+//     {
+//       type: "decrement",
+//     }
+//   );
+// }
+
+// const counter = (state=0, action) => {
+//   switch(action.type)
+//   {
+//     case 'increment': return state + 1;
+//     case 'decrement': return state - 1;
+//     default:;
+//   }
+// }
+
+// let store = createStore(counter);
+
+// store.subscribe(() => {console.log(store.getState())});
+
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
