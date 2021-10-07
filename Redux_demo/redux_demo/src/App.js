233a4d8959a16a4947d36847afeb0457cd07bb74
lythanhhai <you@example.com>
2021-10-07 10:30:24 +0700

    update redux demo

--- Redux_demo/redux_demo/src/App.js
+++ Redux_demo/redux_demo/src/App.js
@@ -1,9 +1,16 @@
-
-
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
+import { increment, decrement } from './actions';
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>hello {counter}</h1>
+      <button onClick={() => {dispatch(increment(5))}}>+</button>
+      <button onClick={() => {dispatch(decrement())}}>-</button>
+      <h3>Valueable information</h3>
     </div>
   );
 }
